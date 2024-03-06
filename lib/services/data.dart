14d6652b4a1795fd6937ba55d3a2d78051193b24
Pratyush Birole <birole.pratyush@gmail.com>
2024-03-06 14:10:56 +0530

    Added data.dart | Category Data

--- lib/services/data.dart
+++ lib/services/data.dart
@@ -1,8 +1,34 @@
 import 'package:newsapp/models/category_model.dart';
 
 List<CategoryModel> getCategories(){
-  List<CategoryMode> category=[];
+  List<CategoryModel> category=[];
   CategoryModel categoryModel= new CategoryModel();
+
   categoryModel.categoryName="Buisness";
   categoryModel.image ="./images";
+  category.add(categoryModel);
+  categoryModel = new CategoryModel();
+
+ categoryModel.categoryName="Entertainment";
+  categoryModel.image ="./images";
+  category.add(categoryModel);
+  categoryModel = new CategoryModel();
+  
+  categoryModel.categoryName="General";
+  categoryModel.image ="./images";
+  category.add(categoryModel);
+  categoryModel = new CategoryModel();
+
+  categoryModel.categoryName="Health";
+  categoryModel.image ="./images";
+  category.add(categoryModel);
+  categoryModel = new CategoryModel();
+
+
+
+  categoryModel.categoryName="Sports";
+  categoryModel.image ="./images";
+  category.add(categoryModel);
+  categoryModel = new CategoryModel();
+
 }
