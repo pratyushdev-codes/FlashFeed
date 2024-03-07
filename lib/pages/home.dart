5869874a9e399e79a807e526e3be638d44e3d9e6
Pratyush Birole <birole.pratyush@gmail.com>
2024-03-07 11:01:45 +0530

    Addinf category Names

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -62,15 +62,34 @@ class CategoryTitle extends StatelessWidget {
 
   CategoryTitle({required this.categoryName, required this.image});
 
-  @override
+   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.only(right: 16),
       child: Stack(
         children: [
-          Image.asset(
-            image,
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Image.asset(
+              image,
+              width: 120,
+              height: 60,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
             width: 120,
             height: 60,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(6),
+              color: Colors.black26,
+            ),
+            child: Center(
+              child: Text(
+                categoryName,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
           ),
         ],
       ),
