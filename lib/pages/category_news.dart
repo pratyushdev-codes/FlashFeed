7062fff9bf9c1ec2e828d5bd8c7250c198a7ed90
Pratyush Birole <birole.pratyush@gmail.com>
2024-03-25 18:09:09 +0530

    Building Category Model

---
+++ lib/pages/category_news.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+class CategoryNews extends StatefulWidget {
+  const CategoryNews({super.key});
+
+  @override
+  State<CategoryNews> createState() => _CategoryNewsState();
+}
+
+class _CategoryNewsState extends State<CategoryNews> {
+  String name;
+  CategoryNews({required this.name});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Colors.black,
+        title: Text(
+                 widget.name;
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                "Feed.",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+        centerTitle: true,
+        elevation: 3.0,
+      ),
+
+      body:Container(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ShowCategory extends StatelessWidget {
+  const ShowCategory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      CategoryModel:
+
+
+    );
+  }
+}
