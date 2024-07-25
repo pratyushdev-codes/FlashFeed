360917f5ba1d26a2623c95c012b001147ab73958
Pratyush Birole <birole.pratyush@gmail.com>
2024-07-25 11:26:10 +0530

    Developing FlashFeed News AI

--- lib/pages/flashfeed_ai.dart
+++ lib/pages/flashfeed_ai.dart
@@ -5,7 +5,32 @@ class FlashfeedAI extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flashfeed AI'),
+        backgroundColor: Colors.black,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Flash",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "Feed",
+              style: TextStyle(
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+        centerTitle: true,
+        elevation: 3.0,
+        iconTheme: IconThemeData(
+          color: Colors.white54, // Change the color of the back button icon
+        ),
+
       ),
       body: Center(
         child: Text('Welcome to Flashfeed AI!'),
